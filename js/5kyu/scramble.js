aac01df70f0dc2e74ec76495e7e88ff0f5e1e728
Eduardo Lustosa <124291493+edulustosa@users.noreply.github.com>
2023-12-28 17:12:55 -0300

    scramble

---
+++ js/5kyu/scramble.js
@@ -0,0 +1,29 @@
+// https://www.codewars.com/kata/55c04b4cc56a697bb0000048/
+
+function scramble(str1, str2) {
+  const letters1 = {};
+  for (let letter of str1) {
+    if (letter in letters1) {
+      letters1[letter] += 1;
+    } else letters1[letter] = 1;
+  }
+
+  const letters2 = {};
+  for (let letter of str2) {
+    if (letter in letters2) {
+      letters2[letter] += 1;
+    } else letters2[letter] = 1;
+  }
+
+  for (let letter in letters2) {
+    if (!(letter in letters1) || letters1[letter] < letters2[letter]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(scramble("katas", "steak"));
+
+
